5653eea8de6a33b0bd4d6e1f74325b8b69eb3d35
borisrosedev <boris.rose.dev@gmail.com>
2025-10-20 17:26:22 +0200

    feat/check

--- main.js
+++ main.js
@@ -1,13 +1,22 @@
 
+
+
+
 window.addEventListener('message', (ev) => {
 
     if(ev.origin !== 'https://art-shop-f.onrender.com') return;
+
     console.log('child receiving', ev.data)
 
-    if(ev.data.msg = 'do it now') {
-        if(window.opener) {
-            window.opener.postMessage({ from: 'child', msg: 'https://google.com'}, 'https://art-shop-f.onrender.com')
-        }
-    }
+
+    const parentWindow = window.opener
+
+    console.log('parentWindow', parentWindow)
+
+    // if(ev.data.msg = 'do it now') {
+    //     if(window.opener) {  
+    //         window.opener.postMessage( { from: 'child', msg: 'https://google.com' }, 'https://art-shop-f.onrender.com')
+    //     }
+    // }
 })
 
